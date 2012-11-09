b6bce9d0209041f20de4bf86abde5e27e1763486
msudhanshu <msudhanshu@kiwiup.com>
2012-11-09 23:06:41 +0530

    first push with android proj

---
+++ collector-android/src/com/collector/MainActivity.java
@@ -0,0 +1,14 @@
+package com.collector;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+
+public class MainActivity extends Activity{//AndroidApplication {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      //  initialize(new GameMain(), false);
+    }
+}
